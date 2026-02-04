1a6bb2cd76ac33e2f50ade92dcf97bd7da4340f8
Adrian <adh323@lehigh.edu>
2026-02-04 07:53:43 -0500

    Update .gitignore to include virtual environment directory and modify requirements.txt to streamline dependencies. Enhance models in max_k_cut with new QUBO model without constraints and clarify function descriptions. Refactor penalty_interpolation notebook for improved readability and structure.

--- max_k_cut/models.py
+++ max_k_cut/models.py
@@ -4,7 +4,7 @@ from docplex.mp.model import Model
 # ======================================================================================================================
 def docplex_BQO(graph, k, name):
     """
-    Create a Max-K-Cut model for the given graph and number of partitions k.
+    Create a Binary Quadratic Optimization (BQO) model for the Max-K-Cut problem.
     """
     # Create the model
     model = Model(name=name)
@@ -38,7 +38,7 @@ def docplex_BQO(graph, k, name):
 # ======================================================================================================================
 def docplex_QUBO(graph, k, penalty, name):
     """
-    Create a Max-K-Cut model for the given graph and number of partitions k.
+    Create a Quadratic Unconstrained Binary Optimization (QUBO) model for the Max-K-Cut problem.
     """
     # Create the model
     model = Model(name=name)
@@ -67,12 +67,41 @@ def docplex_QUBO(graph, k, penalty, name):
 
     return model
 
+#QUBO Model with no constraints (meant to be enforced with a mixer instead)
+# ======================================================================================================================
+def docplex_QUBO_no_constraints(graph, k, name):
+    """
+    Create a Quadratic Unconstrained Binary Optimization (QUBO) model for the Max-K-Cut problem with no constraints.
+    """
+    # Create the model
+    model = Model(name=name)
+
+    # Number of vertices
+    nodes = graph.number_of_nodes()
+    partitions = k
+
+    # Decision variables x_ic (binary) where x_ic = 1 if vertex i is assigned to partition c
+    x = {(i, j): model.binary_var(name=f"x{i}{j}") 
+         for i in range(nodes) for j in range(partitions)}
+
+    # Objective: Maximize the sum of weights of edges between different partitions
+    objective_terms = []
+    for u, v in graph.edges():
+        weight = graph.edges[u, v]['weight']
+        inner_product = model.sum(x[(u, j)] * x[(v, j)] for j in range(partitions))
+        objective_terms.append(weight * (1 - inner_product))
+
+    # Set the objective function to maximize
+    model.maximize(model.sum(objective_terms))
+
+    return model
+
 
 #RBQO Model
 # ======================================================================================================================
 def docplex_RBQO(graph, k, name):
     """
-    Create a Max-K-Cut model for the given graph and number of partitions k.
+    Create a Reduced Binary Quadratic Optimization (RBQO) model for the Max-K-Cut problem.
     """
     # Create the model
     model = Model(name=name)
@@ -107,7 +136,7 @@ def docplex_RBQO(graph, k, name):
 # ======================================================================================================================
 def docplex_RQUBO(graph, k, penalty, name):
     """
-    Create a Max-K-Cut model for the given graph and number of partitions k.
+    Create a Reduced Quadratic Unconstrained Binary Optimization (RQUBO) model for the Max-K-Cut problem.
     """
     # Create the model
     model = Model(name=name)
